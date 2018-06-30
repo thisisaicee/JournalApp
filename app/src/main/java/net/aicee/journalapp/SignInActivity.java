f4362e87bd26c9bc7109857eb7f3cfbc770775f3
Ifeanyichukwu Onyeabor <aicee@aicee.net>
2018-06-30 12:36:47 +0100

    update SignInActivity.java

--- app/src/main/java/net/aicee/journalapp/SignInActivity.java
+++ app/src/main/java/net/aicee/journalapp/SignInActivity.java
@@ -1,4 +1,178 @@
 package net.aicee.journalapp;
 
-public class SignInActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import net.aicee.journalapp.dataModels.User;
+
+
+public class SignInActivity extends BaseActivity implements View.OnClickListener {
+
+    private static final String TAG = "SignInActivity";
+
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
+
+    private EditText emailEditTextField;
+    private EditText passwordEditTextField;
+    private Button signInButton;
+    private Button signUpButton;
+    private com.google.android.gms.common.SignInButton gSignInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Views
+        emailEditTextField = findViewById(R.id.field_email);
+        passwordEditTextField = findViewById(R.id.field_password);
+        signInButton = findViewById(R.id.button_sign_in);
+        signUpButton = findViewById(R.id.button_sign_up);
+        gSignInButton = findViewById(R.id.button_google_sign_in);
+
+
+        // Click listeners
+        signInButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check auth on Activity start
+        if (firebaseAuth.getCurrentUser() != null) {
+            onAuthSuccess(firebaseAuth.getCurrentUser());
+        }
+    }
+
+    private void signIn() {
+        Log.d(TAG, "signIn");
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+        String email = emailEditTextField.getText().toString();
+        String password = passwordEditTextField.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+                        hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(SignInActivity.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signUp() {
+        Log.d(TAG, "signUp");
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+        String email = emailEditTextField.getText().toString();
+        String password = passwordEditTextField.getText().toString();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUser:onComplete:" + task.isSuccessful());
+                        hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(SignInActivity.this, "Sign Up Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+
+        // Write new user
+        writeNewUser(user.getUid(), username, user.getEmail());
+
+        // Go to MainActivity
+        startActivity(new Intent(SignInActivity.this, MainActivity.class));
+        finish();
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(emailEditTextField.getText().toString())) {
+            emailEditTextField.setError("Required");
+            result = false;
+        } else {
+            emailEditTextField.setError(null);
+        }
+
+        if (TextUtils.isEmpty(passwordEditTextField.getText().toString())) {
+            passwordEditTextField.setError("Required");
+            result = false;
+        } else {
+            passwordEditTextField.setError(null);
+        }
+
+        return result;
+    }
+
+    // [START basic_write]
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        databaseReference.child("users").child(userId).setValue(user);
+    }
+    // [END basic_write]
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.button_sign_in) {
+            signIn();
+        } else if (i == R.id.button_sign_up) {
+            signUp();
+        }
+    }
 }
+
