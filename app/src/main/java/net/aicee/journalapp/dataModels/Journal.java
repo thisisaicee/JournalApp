6456e0960c5c3506ee91fb663ad23ea77137c97c
Ifeanyichukwu Onyeabor <aicee@aicee.net>
2018-06-30 08:28:32 +0100

    second commit for Journal.java: set up required constructor and fields

--- app/src/main/java/net/aicee/journalapp/dataModels/Journal.java
+++ app/src/main/java/net/aicee/journalapp/dataModels/Journal.java
@@ -1,4 +1,47 @@
 package net.aicee.journalapp.dataModels;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// [START journal_class]
+@IgnoreExtraProperties
 public class Journal {
+
+    public String uid;
+    public String author;
+    public String title;
+    public String body;
+    public int starCount = 0;
+    public Map<String, Boolean> stars = new HashMap<>();
+
+    public Journal() {
+        // Default constructor required for calls to DataSnapshot.getValue(journal.class)
+    }
+
+    public Journal(String uid, String author, String title, String body) {
+        this.uid = uid;
+        this.author = author;
+        this.title = title;
+        this.body = body;
+    }
+
+    // [START map]
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", uid);
+        result.put("author", author);
+        result.put("title", title);
+        result.put("body", body);
+        result.put("starCount", starCount);
+        result.put("stars", stars);
+
+        return result;
+    }
+    // [END map]
+
 }
+// [END journal_class]
