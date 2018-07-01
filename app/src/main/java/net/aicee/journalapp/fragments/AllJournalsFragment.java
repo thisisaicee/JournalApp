0bb3d50f232b8f2039a39b320d1ea2551ea5e35c
Ifeanyichukwu Onyeabor <aicee@aicee.net>
2018-07-01 06:59:15 +0100

    Updated AllJournalsFragment class file

--- app/src/main/java/net/aicee/journalapp/fragments/AllJournalsFragment.java
+++ app/src/main/java/net/aicee/journalapp/fragments/AllJournalsFragment.java
@@ -1,4 +1,21 @@
 package net.aicee.journalapp.fragments;
 
-public class AllJournalsFragment {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class AllJournalsFragment extends JournalListFragment {
+
+    public AllJournalsFragment() {}
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // [START all_journals_query]
+        // Load all journals
+        String myUserId = getUid();
+        Query allJournalsQuery = databaseReference.child("journals");
+        // [END all_journals_query]
+
+
+        return allJournalsQuery;
+    }
 }
