e4aba78e4bc5263c57b56c6d9827c7a7e5fc9fbf
Ifeanyichukwu Onyeabor <aicee@aicee.net>
2018-07-01 06:58:00 +0100

    updated RecentJournalFragments class file

--- app/src/main/java/net/aicee/journalapp/fragments/RecentJournalsFragment.java
+++ app/src/main/java/net/aicee/journalapp/fragments/RecentJournalsFragment.java
@@ -1,4 +1,20 @@
 package net.aicee.journalapp.fragments;
 
-public class RecentJournalsFragment {
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class RecentJournalsFragment extends JournalListFragment {
+
+    public RecentJournalsFragment() {}
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // [START recent_journals_query]
+        // Load last 20 journals
+        Query recentJournalsQuery = databaseReference.child("journals").limitToFirst(20);
+        // [END recent_journals_query]
+
+        return recentJournalsQuery;
+    }
 }
