6d5678f3a5814d8e501bf86d4f0c69dd71f18aad
Ifeanyichukwu Onyeabor <aicee@aicee.net>
2018-07-01 07:07:57 +0100

    updated MainActivity class file

--- app/src/main/java/net/aicee/journalapp/MainActivity.java
+++ app/src/main/java/net/aicee/journalapp/MainActivity.java
@@ -1,13 +1,89 @@
 package net.aicee.journalapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import net.aicee.journalapp.fragments.AllJournalsFragment;
+import net.aicee.journalapp.fragments.MyJournalsFragment;
+import net.aicee.journalapp.fragments.RecentJournalsFragment;
+
+public class  MainActivity extends BaseActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FragmentPagerAdapter fragmentPagerAdapter;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create the adapter that will return a fragment for each section
+        fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            private final Fragment[] mFragments = new Fragment[] {
+                    new RecentJournalsFragment(),
+                    new MyJournalsFragment(),
+                    new AllJournalsFragment(),
+            };
+            private final String[] mFragmentNames = new String[] {
+                    getString(R.string.heading_recent),
+                    getString(R.string.heading_my_entries),
+                    getString(R.string.heading_my_all_entries)
+            };
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments[position];
+            }
+            @Override
+            public int getCount() {
+                return mFragments.length;
+            }
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mFragmentNames[position];
+            }
+        };
+        // Set up the ViewPager with the sections adapter.
+        viewPager = findViewById(R.id.container);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        // Button launches NewJournalActivity
+        findViewById(R.id.add_new_journal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewJournalActivity.class));
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if (i == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
